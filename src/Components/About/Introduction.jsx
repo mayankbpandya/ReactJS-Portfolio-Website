9fd731bbef52be77d10f5c783603b2f5f4dc8b9e
Mayank Pandya <mayankbpandya@gmail.com>
2023-02-17 19:20:56 +0530

    First commit

---
+++ src/Components/About/Introduction.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Introduction.css";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import { ThemeContext } from "../../Context/theme";
+import profilePic from "../../assets/mayank.webp";
+
+
+export const Introduction = () => {
+  const [{ themename }] = React.useContext(ThemeContext);
+
+  return (
+    <>
+      <section id="#about">
+        <div className="section" data-aos="fade-right">
+          <h2 className="section__title">
+            About <span className="different">Me</span>
+          </h2>
+          <div className={"introduction " + themename}>
+            <div className="introduction_logocontainer">
+              <img src={profilePic} alt="Images" />
+            </div>
+            <div className="introduction_datacontainer">
+              <h4>
+                Hi Everyone, My name is{" "}
+                <span className="different">Mayank Pandya </span> and I am from{" "}
+                <span className="different">
+                  {" "}
+                  Ahmedabad, Gujarat (India)
+                </span>
+                . I have completed my masters in Computer Application
+                from{" "}
+                <span className="different">
+                  Atmiya Institute of Technology & Science, Rajkot (India)
+                </span>
+                . Then I joined full stack development course by{" "}
+                <span className="different">Masai School</span> a military type
+                coding school.
+              </h4>
+              <h4>Terms That can describe me apart form Coding :</h4>
+              <h4 className="different">
+                <span className="icons">
+                  <ExitToAppIcon />
+                </span>
+                Entrepreneure{" "}
+              </h4>
+              <h4 className="different">
+                <span className="icons">
+                  <ExitToAppIcon />
+                </span>
+                Chess Enthusiast{" "}
+              </h4>
+              <h4 className="different">
+                <span className="icons">
+                  <ExitToAppIcon />
+                </span>
+                YouTuber{" "}
+              </h4>
+              <h4 className="different">
+                <span className="icons">
+                  <ExitToAppIcon />
+                </span>
+                Poet{" "}
+              </h4>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Contact.css";
+import { VscGithub } from "react-icons/vsc";
+import { CgMail } from "react-icons/cg";
+import { BsInstagram, BsFillTelephoneFill } from "react-icons/bs";
+import { ThemeContext } from "../../Context/theme";
+
+import { FaLinkedin } from "react-icons/fa";
+export const Contact = () => {
+  const [{ themename }] = React.useContext(ThemeContext);
+
+  return (
+    <>
+      <div className="section">
+        <h2 className="section__title" data-aos="fade-right">
+          Get in <span className="different">Touch</span>
+        </h2>
+        <div className="contactMain">
+          <div
+            className={"contactcontainer " + themename}
+            data-aos="fade-right"
+          >
+            <a
+              href="https://www.linkedin.com/in/mayank-pandya/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaLinkedin className="linkedin" />
+            </a>
+            <a
+              href="https://github.com/mayankbpandya"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <VscGithub className="github" />
+            </a>
+            <a
+              href="mailto:mayankbpandya@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <CgMail className="email" />
+            </a>
+            <a href="tel:+919033511224" target="_blank" rel="noreferrer">
+              <BsFillTelephoneFill className="phone" />
+            </a>
+          </div>
+          <div className="mailNumber" data-aos="fade-right">
+            <div>
+              <span>
+                <CgMail className="email" />
+              </span>
+              <span>
+                <p>mayankbpandya@gmail.com</p>
+              </span>
+            </div>
+            <div className="iconsCont">
+              <span>
+                <BsFillTelephoneFill className="phone" />
+              </span>
+              <span>
+                <p>+91 9033511224</p>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
